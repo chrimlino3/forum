cb1d125e420b669926a8ba566baf6b819344c842
Christina Meliniotou <christina.meliniotou@yource.com>
2020-12-08 18:41:37 +0100

    packages

--- app/routes.php
+++ app/routes.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::name('admin.')->group(function () {
+Route::group(['middleware' => ['role:admin']], function () {
     Route::resource('reply', \App\Http\Controllers\ReplyController::class);
 });
